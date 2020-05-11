08aea1601d461343cb9b43faced1a09d12157ae5
Danio <danio0@onet.pl>
2020-05-11 08:26:00 +0200

    add mew foles

---
+++ wędkuj.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+   <title>Wędkujemy</title>
+    <meta charset="utf-8">
+ <link rel="stylesheet" href="styl_1.css">
+</head>
+<body>
+  <header>
+    <h1>Portal dla wędkarzy</h1>
+  </header>
+
+  <main>
+    <article>
+      <h3>Ryby drapieżne naszych wód</h3>
+
+    </article>
+	
+	<aside>
+      <img src="./ryba1.jpg" alt="Sum"/>
+	  <ol>
+	  <li><a href="kwerendy.txt" target="_blank">Pobierz kwerendy</a></li>
+    </aside> 
+	
+  </main>
+
+  <footer>
+    <p>Stronę wykonał:000000000</p>
+  </footer>
+</body>
+</html>
